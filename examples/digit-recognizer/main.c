a3176f8fa5cf08b3e24172358694143f165fa8f9
HearthWell <hearthvvell@gmail.com>
2024-08-29 14:30:58 -0400

    updated the digit-recognizer example and added readme

--- examples/digit-recognizer/main.c
+++ examples/digit-recognizer/main.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
 #include "mlp.h"
+#include "dataset.h"
 
-#define INPUT_IMG_SIZE 26
+#define INPUT_IMG_SIZE 28
 #define HIDDEN_DIM 16
 #define OUTPUT_DIM 10
+
+/* TODO, CHANGE FOR YOUR OWN PATH */
+#define TRAINING_DATASET_DIR "~/datasets/MNIST/training"
+#define VALIDATION_DATASET_DIR "~/datasets/MNIST/validation"
 
 int main(){
     printf("DIGIT-RECOGNIZER EXAMPLE\n");
     
     struct mlp network = mlp_init(INPUT_IMG_SIZE * INPUT_IMG_SIZE);
-    
     mlp_add_layer(&network, HIDDEN_DIM);
     mlp_add_layer(&network, HIDDEN_DIM);
     mlp_add_layer(&network, OUTPUT_DIM);
 
     mlp_print(&network); 
+
+    const unsigned int num_epochs = 1;
+
+    struct Dataset dataset = mlp_dataset_mnist_init(TRAINING_DATASET_DIR);
+    mlp_train(&network, &dataset, NULL, mlp_default_optimizer(), num_epochs);
+    /* STILL UNDER DEVELOPMENT, SINGLE THREADED IS THE RECOMMENDED WAY FOR NOW */
+    //mlp_train_threaded(&network, &dataset, NULL, mlp_default_optimizer(), num_epochs, 2);
+    dataset.free(&dataset);
 
     mlp_free(&network);
 
---
+++ examples/digit-recognizer/readme.md
@@ -0,0 +1,29 @@
+# Mnist Dataset Structure
+
+```bash
+/data
+    mnist
+        training
+            0
+                img_1.jpg
+                img_2.jpg
+                ...
+            1
+                img_1.jpg
+                img_2.jpg
+                ...
+            ...
+        validation
+            0
+                img_1.jpg
+                img_2.jpg
+                ...
+            1
+                img_1.jpg
+                img_2.jpg
+                ...
+            ...
+```
+
+### The folder 0 contains images of the digit 0 and every digits has such a folder in the training and validation directories.
+            
